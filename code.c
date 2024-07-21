ba343fad576d1b4a364d37e831e04764c751ab37
zshtt <127484354+zshtt@users.noreply.github.com>
2024-07-21 15:00:11 +0800

    云端直接篡改

--- code.c
+++ code.c
@@ -1,15 +1,8 @@
 #include<stdio.h>
 
 int main(void){
-        printf("code 1");
-  printf("code2 :" );
-  printf("code3 :" );
-  printf("code4 :" );
-  printf("code 5:" );
-  printf("code6 :" );
-  printf("code7 :" );
-  printf("code8 :" );
-
+        printf("change \n");
+ 
 
 
 
