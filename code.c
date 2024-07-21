b1667306ed4df28b94c271f624b6f5929d3400ca
zshtt <z15146765580@163.com>
2024-07-21 14:36:10 +0800

    修改提交

--- code.c
+++ code.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 
 int main(void){
-	printf("code :" );
-	return0;
+        printf("code 1");
+  printf("code2 :" );
+  printf("code3 :" );
+  printf("code4 :" );
+  printf("code 5:" );
+  printf("code6 :" );
+  printf("code7 :" );
+  printf("code8 :" );
+
+
+
+
+
+
+
+
+
+
+	return 0;
 }
